dcb5a3ecaeb65f0f2e30bc5a78230f12d9e0588c
eugene <evgeniy.pismenny@gmail.com>
2015-02-15 00:03:17 +0200

    add interface RandomizedQueue

---
+++ src/main/java/RandomizedQueue.java
@@ -0,0 +1,42 @@
+import java.util.Iterator;
+
+/**
+ * Created by eugene on 14.02.15.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+
+    public RandomizedQueue() {
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item dequeue() {
+        return null;
+    }
+
+    public Item sample() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
