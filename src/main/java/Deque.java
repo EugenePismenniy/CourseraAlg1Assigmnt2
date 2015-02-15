263d51413ad40f216013172c30a4f814b317547d
eugene <evgeniy.pismenny@gmail.com>
2015-02-16 00:26:56 +0200

    first version collections

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -1,41 +1,67 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * Created by eugene on 14.02.15.
  */
 public class Deque<Item> implements Iterable<Item> {
 
+    private LinkedList<Item> list = new LinkedList<Item>();
+
     public Deque() {
     }
 
-
     public boolean isEmpty() {
-        return false;
+        return list.isEmpty();
     }
 
     public int size() {
-        return 0;
+        return list.size();
     }
 
     public void addFirst(Item item) {
-
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        list.addFirst(item);
     }
 
     public void addLast(Item item) {
-
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        list.addLast(item);
     }
 
     public Item removeFirst() {
-        return null;
+        return list.removeFirst();
     }
 
     public Item removeLast() {
-        return null;
+        return list.removeLast();
     }
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new Iterator<Item>() {
+
+            private Iterator<Item> iterator = list.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Item next() {
+                return iterator.next();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 
     public static void main(String[] args) {
--- src/main/java/RandomizedQueue.java
+++ src/main/java/RandomizedQueue.java
@@ -1,42 +1,118 @@
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 /**
  * Created by eugene on 14.02.15.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+    private LinkedList<Item> list = new LinkedList<Item>();
 
     public RandomizedQueue() {
     }
 
     public boolean isEmpty() {
-        return false;
+        return list.isEmpty();
     }
 
     public int size() {
-        return 0;
+        return list.size();
     }
 
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
 
+        list.add(item);
     }
 
     public Item dequeue() {
-        return null;
+
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        int i = randomIndex();
+        return list.remove(i);
     }
 
     public Item sample() {
-        return null;
+
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        return list.get(randomIndex());
+    }
+
+    private int randomIndex() {
+        return StdRandom.uniform(0, list.size());
     }
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+
+        return new Iterator<Item>() {
+            {
+                Object[] tmp = list.toArray();
+                StdRandom.shuffle(tmp);
+                array = tmp;
+            }
+            private int i = 0;
+            private Object[] array;
+
+            @Override
+            public boolean hasNext() {
+                return i < array.length;
+            }
+
+            @Override
+            public Item next() {
+
+                if (hasNext())
+                    return (Item) array[i++];
+
+                throw new NoSuchElementException();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 
     public static void main(String[] args) {
 
+
+        RandomizedQueue<String> list = new RandomizedQueue<String>();
+
+        list.enqueue("A");
+        list.enqueue("B");
+        list.enqueue("C");
+        list.enqueue("D");
+
+
+        System.out.println("----------- " + list.size() + "-------------");
+
+        for (String e : list) {
+            System.out.println(e);
+        }
+
+
+        System.out.println("--------------------");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(list.sample());
+        }
+
+        System.out.println("-----------" + list.size() + "----------");
+        int n = list.size();
+        for (int i = 0; i < n+1; i++) {
+            System.out.println(list.dequeue());
+        }
+
+        //System.out.println(list.dequeue());
     }
-
-
 }
